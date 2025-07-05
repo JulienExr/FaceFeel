5159de2b682ec6038f5c6e01c64220fcae2c71c5
Julien <julien.excoffier@ecole.ensicaen.fr>
2025-07-05 10:22:38 +0200

    Add DataLoader implementation

---
+++ src/dataloader.py
@@ -0,0 +1,55 @@
+import os
+import torchvision.transforms as transforms
+import torchvision.datasets
+from torch.utils.data import DataLoader
+
+def get_dataloader(data_dir, image_size=48, batch_size=64):
+    """
+    Create a DataLoader for the dataset in the specified directory.
+
+    Args:
+        data_dir (str): Path to the directory containing the dataset.
+        image_size (int): Size to which images will be resized.
+        batch_size (int): Number of samples per batch.
+
+    Returns:
+        train_loader, val_loader, test_loader: DataLoaders for training, validation, and testing datasets.
+    """
+
+    common_transforms = transforms.Compose([
+    transforms.Resize((image_size, image_size)),
+    transforms.Grayscale(num_output_channels=1),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5], std=[0.5])
+    ])
+
+    train_dataset = torchvision.datasets.ImageFolder(
+        root=os.path.join(data_dir, 'train'),
+        transform=common_transforms
+        )
+    val_dataset = torchvision.datasets.ImageFolder(
+        root=os.path.join(data_dir, 'val'),
+        transform=common_transforms
+        )
+    test_dataset = torchvision.datasets.ImageFolder(
+        root=os.path.join(data_dir, 'test'),
+        transform=common_transforms
+        )
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True
+        )
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=False
+        )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False
+        )
+    
+    return train_loader, val_loader, test_loader
